391f0f1aac7ca1895d6105c2821ccf3150f5b5b5
Morten Grouleff <mgr@humio.com>
2019-06-27 10:33:31 +0200

    More test cases that fail

--- src/test/java/com/humio/jitrex/LargeInputTest.java
+++ src/test/java/com/humio/jitrex/LargeInputTest.java
@@ -22,8 +22,57 @@ public class LargeInputTest {
 
     @Test
     public void testInputWithLargeConstantParts() {
-        for (int i = 0; i < 100; i++) {
-            String s = makeInput(i, 500);
+        for (int i = 0; i < 1000; i++) {
+            String s = makeInput(i, 50);
+            try {
+                Pattern.compile(s);
+            } catch (Throwable e) {
+                throw new RuntimeException("Failed at input length=" + s.length() + " from i=" + i, e);
+            }
+        }
+    }
+
+
+
+    private String makeDotAllInput(int repetitions, int constantLen) {
+        StringBuilder sb = new StringBuilder(repetitions * constantLen);
+        for (int i = 0; i < repetitions; i++) {
+            sb.append("(");
+            for (int j = 0; j < constantLen; j++) {
+                sb.append('.');
+            }
+            sb.append(")");
+        }
+        return sb.toString();
+    }
+
+    @Test
+    public void testInputWithLargeAnyParts() {
+        for (int i = 0; i < 1000; i++) {
+            String s = makeDotAllInput(i, 50);
+            try {
+                Pattern.compile(s);
+            } catch (Throwable e) {
+                throw new RuntimeException("Failed at input length=" + s.length() + " from i=" + i, e);
+            }
+        }
+    }
+
+
+    private String makeManyGlobsInput(int repetitions, int multiplier) {
+        StringBuilder sb = new StringBuilder(repetitions * multiplier);
+        for (int i = 0; i < repetitions; i++) {
+            for (int j = 0; j < multiplier; j++) {
+                sb.append("(.*),");
+            }
+        }
+        return sb.toString();
+    }
+
+    @Test
+    public void testInputWithManyGlobs() {
+        for (int i = 0; i < 1000; i++) {
+            String s = makeManyGlobsInput(i, 1);
             try {
                 Pattern.compile(s);
             } catch (Throwable e) {
